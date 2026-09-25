66a6ad894172e718b6f002a26041b0af2dfdb23b
Gaurav Bhatia <gauravbhatia2190@gmail.com>
2026-09-25 11:21:56 +0530

    feat: place matchmaker on homepage and move community reviews up

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import agents from '../data/agents.json';
 import AppShell from '../components/AppShell';
 import LivePulse from '../components/LivePulse';
 import NewsGrid from '../components/NewsGrid';
+import Matchmaker from '../components/Matchmaker';
 import AgentExplorer from '../components/AgentExplorer';
-import CategoriesGrid from '../components/CategoriesGrid';
 import CommunityReviews from '../components/CommunityReviews';
+import CategoriesGrid from '../components/CategoriesGrid';
 import NewsletterSignup from '../components/NewsletterSignup';
 
 export default async function Home({
@@ -19,9 +20,10 @@ export default async function Home({
     <AppShell rightRail={<LivePulse />}>
       <div className="px-4 lg:px-8 py-8 max-w-[1100px] mx-auto space-y-12">
         <NewsGrid />
+        <Matchmaker agents={agents} />
         <AgentExplorer agents={agents} initialQuery={q} />
-        <CategoriesGrid />
         <CommunityReviews />
+        <CategoriesGrid />
         <NewsletterSignup />
       </div>
     </AppShell>
